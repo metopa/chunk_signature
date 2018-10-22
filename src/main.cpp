296f268f61cb0b12e008f58bdb57e042cb13459f
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-10-22 15:38:45 +0200

    Add processing loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
+#include "file_chunker.h"
+#include "hasher/base_hasher.h"
 #include "program_config.h"
 #include "result_writer/m_mapped_result_writer.h"
-#include "file_chunker.h"
 
 
 int main(int argc, char* argv[]) {
@@ -10,17 +11,14 @@ int main(int argc, char* argv[]) {
 		auto config = ProgramConfig::fromCommandLine(argc, argv);
 		std::cout << config << std::endl;
 
+		auto backend = BaseHasher::getBackendByName(config.backend_name);
 		FileChunker input(config.input_file, config.chunk_size);
-		MMappedResultWriter writer(config.output_file, input.chunkCount(), config.chunk_size);
+		MMappedResultWriter writer(config.output_file, input.chunkCount(), backend->hashSize());
 
 		for (size_t i = 0; i < input.chunkCount(); i++) {
 			auto chunk = input.getChunk(i);
-			if (chunk.size == config.chunk_size) {
-				hash_result_t h((unsigned char*) chunk.data,
-								(unsigned char*) chunk.data + chunk.size);
-
-				writer.writeResult(h, i);
-			}
+			auto hash = backend->calculateHash(chunk.data, chunk.size);
+			writer.writeResult(hash, i);
 		}
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
